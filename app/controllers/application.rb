4965aadf467c7081ce324afb8f345b5d2aa55b31
Your Name <muneshwarketan@gmail.com>
2009-12-08 00:25:37 +0530

    A;WWWWWWWWWWWWWWWWWWWWWWWW

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,5 +1,5 @@
 # Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers cdwfqwqwrgerg.
+# sfwefLikewise, all the methods added will be available for all controllers cdwfqwqwrgerg.
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
